dddf96834a6ddd3f1d7caf2138da44886801de02
sebfrab <seb.frab@gmail.com>
2014-08-20 16:30:38 -0400

    ajustes

--- index.php
+++ index.php
@@ -9,6 +9,17 @@
         <meta name="description" content="Cerveceria Andinez" />
         <meta http-equiv="X-UA-Compatible" content="IE=7,8,9" /> 
         
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-54049000-1', 'auto');
+            ga('send', 'pageview');
+
+        </script>
+        
         <!-- Bootstrap 3.0 -->
         <link rel="stylesheet" type="text/css" href="./css/bootstrap.css" />
         <!-- Normalización para Web -->
